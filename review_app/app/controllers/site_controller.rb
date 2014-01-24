2b7d76176df442ef17c0b1dfffad38ae912049fe
David Simons <dave.c.simons@gmail.com>
2014-01-23 17:52:44 -0800

    In class work from Week 3, Day 4

---
+++ review_app/app/controllers/site_controller.rb
@@ -0,0 +1,16 @@
+class SiteController < ApplicationController
+
+	def index
+
+	end
+
+	def contact
+
+	end
+
+	def about
+
+	end
+
+
+end	
---
+++ review_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReviewApp::Application.config.secret_key_base = '5718c044836d6a1542331acd868b282c3b6012e584a907c4716d82959ac9db5a69663fa0e55194276bc42fdddff82e73290073d69e4bb7475d8dcb61db6cb5f8'
